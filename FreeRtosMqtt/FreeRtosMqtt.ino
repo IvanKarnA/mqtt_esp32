83188252b308427ebdcfd548be4e26476bbe819b
Ivan K <ivan03.05.2005@gmail.com>
2023-02-24 09:00:01 +0400

    freeRTOS work

--- FreeRtosMqtt/FreeRtosMqtt.ino
+++ FreeRtosMqtt/FreeRtosMqtt.ino
@@ -66,7 +66,7 @@ void setup()
   Serial.begin(115200);
   WifiConnect();
   xTaskCreate(MQTTClientTask,"MQTTTask",3*1024,NULL,1,NULL);
-  vTaskStartScheduler();
+  
   // client.publish("esp/test","123");
 }
 
